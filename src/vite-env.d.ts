f171b377154de1a5f6a83ba37cfdf2e036858df2
boldmonk89 <tejascodes2006@gmail.com>
2026-04-24 09:30:21 +0530

    Fix: Migrate from gsap-trial to free standard gsap package

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
 
-declare module 'gsap-trial/SplitText';
-declare module 'gsap-trial/all';
+declare module 'gsap/SplitText';
+declare module 'gsap/all';
+declare module 'gsap/ScrollSmoother';
